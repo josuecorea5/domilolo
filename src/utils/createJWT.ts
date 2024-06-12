7714dac8d347fbf16dca1c2428ed57ced9a7bdac
Josue Coreas <josuepaiz487@gmail.com>
2024-06-12 14:07:24 -0600

    Fixing env variable bug 🐛

--- src/utils/createJWT.ts
+++ src/utils/createJWT.ts
@@ -2,10 +2,10 @@ import jsonwebtoken from "jsonwebtoken";
 import { User } from "../interfaces/user";
 
 export const createJWT = (user: User) => {
-  const token = jsonwebtoken.sign({ id: user.id, username: user.username, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
+  const token = jsonwebtoken.sign({ id: user.id, username: user.username, role: user.role }, process.env.JWT_SECRET as string, { expiresIn: "1h" });
   return token;
 }
 
 export const verifyJWT = (token) => {
-  return jsonwebtoken.verify(token, process.env.JWT_SECRET);
+  return jsonwebtoken.verify(token, process.env.JWT_SECRET as string);
 }
